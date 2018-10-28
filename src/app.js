d57da137ae6a9a178c43f47cbc18f142c6bb8140
davisboy <ddavis@path.org>
2018-10-28 16:52:38 -0700

    Completed module

--- src/app.js
+++ src/app.js
@@ -4,14 +4,10 @@ const express = require('express');
 
 const app = express();
 
-app.set('views', path.join(__dirname, '/views/'));
 app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname, '/public/')));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', function(req, res){
-    res.render('index', {title: 'Index'});
-});
+app.get('/', (req, res) => res.render('index', {title: 'Index'}));
 
-app.listen(3000, function(){
-    console.log('PS Project Running on port 3000!')
-});
+app.listen(3000, () => console.log('PS Project Running on port 3000!'));
